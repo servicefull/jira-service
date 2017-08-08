0387f3e462dbb21a62bdc5662f68a973f28a17fa
servicefull <rob.bowers@gmail.com>
2017-08-08 16:34:29 -0400

    index mods

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict';
 
-const request = require('request');
-
 exports.handler = (event, context, callback) => {
+
+    // PARSE MESSAGE
+	const p = event.Records[0].Sns.Message;
+	const payload = JSON.parse(p);
+
+	const request = require('request');
 	const account = process.env.JIRA_ACCOUNT;
 	const username1 = process.env.JIRA_USER;
 	const password1 = process.env.JIRA_PASS;
@@ -12,29 +16,27 @@ exports.handler = (event, context, callback) => {
 	const apiUrl = "https://"+account+".atlassian.net/rest/api/2/issue/";
 	const auth = "Basic " + new Buffer(username1 + ":" + password1).toString("base64");
 
+	const postData = {
+    	"fields": {
+    		"project": {
+    			"key": project_key
+    		},
+    		"summary": "QA Page : "+payload.pageUrl+" : "+payload.size,
+    		"description": "# QA Page : (screenshot loacted here: https://s3.amazonaws.com/"+payload.s3Bucket+"/"+payload.fileName+")",
+    		"issuetype": {
+    			"id": issue_type
+    		}
+    	}
+	}
+
 	request({
 		url: apiUrl,
 		headers: {
 			"Authorization": auth
 		},
 		method: 'POST',
-		json: {
-			"fields": {
-				"project": {
-					"key": project_key
-				},
-				"summary": "QA Page",
-				"description": "# Create new Page",
-				"issuetype": {
-					"id": issue_type
-				}
-			}
-		}
+		json: postData
 	}, (error, resp, body) => {
-		if (body.errors) {
-			console.log('error',body.errors);
-		} else {
-			console.log('success');
-		}
+		callback(null, 'success');
 	});
 };
